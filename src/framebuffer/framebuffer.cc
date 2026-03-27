9f2042f4cd482e2155b8f4b31b355deb6825cc30
Kubik <kubik.votr@gmail.com>
2026-03-27 08:50:11 +0100

    Framebuffer updated: Clear(), std::span, PixelIdx(),...

--- src/framebuffer/framebuffer.cc
+++ src/framebuffer/framebuffer.cc
@@ -1,29 +1,54 @@
 #include "framebuffer/framebuffer.h"
 
 #include <cassert>
+#include <cstring>
+#include <span>
+
+#include "util/vec3.h"
 
 namespace diplodocus {
 
 Framebuffer::Framebuffer(int width, int height) : width_(width), height_(height), data_(width * height * 3) {}
 
-void Framebuffer::SetPixel(int x, int y, const Vec3& c) {
-    assert(x < width_ && x >= 0 && "Framebuffer::SetPixel: out-of-bounds");
-    assert(y < width_ && y >= 0 && "Framebuffer::SetPixel: out-of-bounds");
-
-    int color_idx = (y * width_ + x) * 3;
-    data_[color_idx + 0] = c.x;
-    data_[color_idx + 1] = c.y;
-    data_[color_idx + 2] = c.z;
-}
-
-Vec3 Framebuffer::GetPixel(int x, int y) const {
-    return {data_[y * width_ + x + 0], data_[y * width_ + x + 1], data_[y * width_ + x + 2]};
-}
-
 int Framebuffer::GetWidth() const { return width_; }
 
 int Framebuffer::GetHeight() const { return height_; }
 
-const std::vector<float>& Framebuffer::GetData() const { return data_; }
+Vec3 Framebuffer::GetPixel(int x, int y) const {
+    int pixel_idx = PixelIdx(x, y);
+    return {data_[pixel_idx + 0], data_[pixel_idx + 1], data_[pixel_idx + 2]};
+}
+
+std::span<const float> Framebuffer::GetData() const { return std::span<const float>(data_.begin(), data_.size()); }
+
+void Framebuffer::Resize(int width, int height) {
+    width_ = width;
+    height_ = height;
+    data_.resize(width_ * height_ * 3);
+}
+
+void Framebuffer::SetPixel(int x, int y, const Vec3& c) {
+    int pixel_idx = PixelIdx(x, y);
+    data_[pixel_idx + 0] = c.x;
+    data_[pixel_idx + 1] = c.y;
+    data_[pixel_idx + 2] = c.z;
+}
+
+void Framebuffer::Clear(const Vec3& color) {
+    const int pixels = width_ * height_;
+    for (int i = 0; i < pixels; i++) {
+        int pixel_idx = i * 3;
+        data_[pixel_idx + 0] = color.x;
+        data_[pixel_idx + 1] = color.x;
+        data_[pixel_idx + 2] = color.x;
+    }
+}
+
+int Framebuffer::PixelIdx(int x, int y) const {
+    assert(x < width_ && x >= 0 && "Framebuffer: out-of-bounds");
+    assert(y < height_ && y >= 0 && "Framebuffer: out-of-bounds");
+
+    return (y * width_ + x) * 3;
+}
 
 }  // namespace diplodocus
--- src/framebuffer/framebuffer.h
+++ src/framebuffer/framebuffer.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <span>
 #include <vector>
 
+#include "util/colors.h"
 #include "util/vec3.h"
 
 namespace diplodocus {
@@ -10,15 +12,20 @@ class Framebuffer {
    public:
     Framebuffer(int width = 0, int height = 0);
 
-    void SetPixel(int x, int y, const Vec3& c);
-    Vec3 GetPixel(int x, int y) const;
     int GetWidth() const;
     int GetHeight() const;
-    const std::vector<float>& GetData() const;
+    Vec3 GetPixel(int x, int y) const;
+    std::span<const float> GetData() const;
+
+    void Resize(int width, int height);
+    void SetPixel(int x, int y, const Vec3& c);
+    void Clear(const Vec3& color = color::kBlack);
 
    private:
     int width_, height_;
     std::vector<float> data_;
+
+    int PixelIdx(int x, int y) const;
 };
 
 }  // namespace diplodocus

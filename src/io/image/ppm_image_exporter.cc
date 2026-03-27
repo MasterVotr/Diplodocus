43867c784ec5a9424d8453dbca15fc861cf24c37
Kubik <kubik.votr@gmail.com>
2026-03-27 08:54:54 +0100

    ppmimage exporter removed unused virtual specifier, added error logging and cleand up

--- src/io/image/ppm_image_exporter.cc
+++ src/io/image/ppm_image_exporter.cc
@@ -1,5 +1,6 @@
 #include "io/image/ppm_image_exporter.h"
 
+#include <algorithm>
 #include <fstream>
 
 #include "util/logger.h"
@@ -7,16 +8,23 @@
 namespace diplodocus {
 
 void PpmImageExporter::Export(const ImageExporterConfig& config, const Framebuffer& framebuffer) const {
-    int w = framebuffer.GetWidth();
-    int h = framebuffer.GetHeight();
+    const int w = framebuffer.GetWidth();
+    const int h = framebuffer.GetHeight();
     const auto& data = framebuffer.GetData();
+
     std::ofstream output(config.filepath.c_str());
+    if (!output) {
+        Logger::error("PPM Image exporter: Could not write to file {}!", config.filepath.c_str());
+        return;
+    }
+
+    const auto to_u8 = [](float v) { return static_cast<int>(255.999f * std::clamp(v, 0.0f, 1.0f)); };
 
     output << "P3\n" << framebuffer.GetWidth() << ' ' << framebuffer.GetHeight() << "\n255\n";
     for (int i = 0; i < w * h; i++) {
-        output << static_cast<int>(255.999 * (data[i * 3 + 0])) << ' ';
-        output << static_cast<int>(255.999 * (data[i * 3 + 1])) << ' ';
-        output << static_cast<int>(255.999 * (data[i * 3 + 2])) << '\n';
+        output << to_u8(data[i * 3 + 0]) << ' ';
+        output << to_u8(data[i * 3 + 1]) << ' ';
+        output << to_u8(data[i * 3 + 2]) << '\n';
     }
 
     output.close();
--- src/io/image/ppm_image_exporter.h
+++ src/io/image/ppm_image_exporter.h
@@ -6,7 +6,7 @@ namespace diplodocus {
 
 class PpmImageExporter : public ImageExporter {
    public:
-    virtual void Export(const ImageExporterConfig& config, const Framebuffer& framebuffer) const override;
+    void Export(const ImageExporterConfig& config, const Framebuffer& framebuffer) const override;
 };
 
 }  // namespace diplodocus

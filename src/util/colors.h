c3124210d7b3026a989cfcc2ffb7ff94635e5850
Kubik <kubik.votr@gmail.com>
2026-03-27 08:53:00 +0100

    Utils added: colors, hashing, float comparison

--- src/util/colors.h
+++ src/util/colors.h
@@ -7,9 +7,18 @@ namespace color {
 
 constexpr Vec3 kBlack = Vec3{0.0f};
 constexpr Vec3 kWhite = Vec3{1.0f};
+
+constexpr Vec3 kGray = Vec3{0.5f};
+constexpr Vec3 kLightGray = Vec3{0.75f};
+constexpr Vec3 kDarkGray = Vec3{0.25f};
+
 constexpr Vec3 kRed = Vec3{1.0f, 0.0f, 0.0f};
 constexpr Vec3 kGreen = Vec3{0.0f, 1.0f, 0.0f};
 constexpr Vec3 kBlue = Vec3{0.0f, 0.0f, 1.0f};
+
+constexpr Vec3 kCyan = Vec3{0.0f, 1.0f, 1.0f};
+constexpr Vec3 kMagenta = Vec3{1.0f, 0.0f, 1.0f};
+constexpr Vec3 kYellow = Vec3{1.0f, 1.0f, 0.0f};
 
 }  // namespace color
 
--- src/util/util.h
+++ src/util/util.h
@@ -9,6 +9,27 @@
 const float kEpsilon = 1e-3;
 const float kInfinity = std::numeric_limits<float>::max();
 
+// Comparison
+[[nodiscard]] inline bool AlmostEqual(float a, float b, float eps = kEpsilon) noexcept {
+    return std::fabs(a - b) < eps;
+}
+
+// Hashing
+[[nodiscard]] inline uint32_t HashU32(uint32_t x) {
+    // Simple, fast avalanche hash (good enough for sampling)
+    x ^= x >> 16;
+    x *= 0x7feb352dU;
+    x ^= x >> 15;
+    x *= 0x846ca68bU;
+    x ^= x >> 16;
+    return x;
+}
+
+[[nodiscard]] static inline float U01FromU32(uint32_t x) {
+    // Use the top 24 bits -> exactly representable steps in float
+    return (x >> 8) * (1.0f / 16777216.0f); // 2^24
+}
+
 // Printing
 template <typename T>
 inline std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {

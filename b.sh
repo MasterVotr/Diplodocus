b6582a89c425706f2f44a04163744190165ffccd
Kubik <kubik.votr@gmail.com>
2026-04-10 14:36:14 +0200

    Forcing clang as compiler and added CUDA compilation to cmake

--- b.sh
+++ b.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cmake -S . -B build
+cmake -S . -B build \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
+  -DCMAKE_CXX_FLAGS="-stdlib=libc++"
 cmake --build build --parallel
 

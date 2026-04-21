263b3ad7c843c2b556728118836b17b87047f464
Kubik <kubik.votr@gmail.com>
2026-04-21 16:09:51 +0200

    Added testing scenes

--- src/app/cli_app.cc
+++ src/app/cli_app.cc
@@ -12,9 +12,166 @@
 #include "io/stats/stats_exporter_factory.h"
 #include "renderer/cpu_raytracer.h"
 #include "renderer/gpu_renderer.h"
+#include "scene/scene.h"
+#include "scene/vertex.h"
+#include "util/colors.h"
 #include "util/logger.h"
+#include "util/vec3.h"
 
 namespace diplodocus {
+
+namespace {
+
+Vec3 CalculateCameraUp(const Vec3& from, const Vec3& to, const Vec3& arbitrary_up) {
+    Vec3 forward = Normalize(from - to);
+    Vec3 right = Normalize(Cross(arbitrary_up, forward));
+    return Cross(forward, right);
+}
+
+Scene CreateTestScene1() {
+    Logger::debug("Creating a test scene 1 (quad)");
+    Scene test_scene{};
+
+    // Materials
+    Material m0;
+    m0.diffuse = color::kRed;
+    Material m1;
+    m1.diffuse = color::kBlue;
+    test_scene.AddMaterial(m0);
+    test_scene.AddMaterial(m1);
+
+    // Triangles
+    Vertex vs[5];
+    vs[0].pos = {0.0f, 0.0f, 0.0f};
+    vs[1].pos = {1.0f, 0.0f, 0.0f};
+    vs[2].pos = {0.0f, 1.0f, 0.0f};
+    vs[3].pos = {1.0f, 1.0f, 0.0f};
+    Triangle t0;
+    t0.v0 = vs[0];
+    t0.v1 = vs[3];
+    t0.v2 = vs[2];
+    t0.material_id = 0;
+    Triangle t1;
+    t1.v0 = vs[0];
+    t1.v1 = vs[1];
+    t1.v2 = vs[3];
+    t1.material_id = 1;
+    test_scene.AddTriangle(t0);
+    test_scene.AddTriangle(t1);
+
+    // Lights
+    PointLight p;
+    p.color = color::kWhite;
+    p.pos = {0.5f, 1.5f, 1.0f};
+    test_scene.AddPointLight(p);
+
+    // Camera
+    Camera& c = test_scene.GetCamera();
+    c.pos = {0.5f, 0.5f, 2.0f};
+    c.dir = {0.0f, 0.0f, -1.0f};
+    c.up = {0.0f, 1.0f, 0.0f};
+    assert(c.up == CalculateCameraUp(c.pos, c.pos + c.dir, c.up));
+    c.far = 1e3;
+    c.fov = 0.6;
+
+    return test_scene;
+}
+
+Scene CreateTestScene2() {
+    Logger::debug("Creating a test scene 2 (octahedron)");
+    Scene test_scene{};
+
+    // Materials
+    Material red{};
+    red.diffuse = color::kRed;
+    Material blue{};
+    blue.diffuse = color::kBlue;
+    Material green{};
+    green.diffuse = color::kGreen;
+    Material yellow{};
+    yellow.diffuse = color::kYellow;
+    test_scene.AddMaterial(red);
+    test_scene.AddMaterial(blue);
+    test_scene.AddMaterial(green);
+    test_scene.AddMaterial(yellow);
+
+    // Triangles
+    Vertex vs[6];
+    vs[0].pos = {0.5f, 1.0f, 0.5f};
+    vs[1].pos = {0.5f, 0.0f, 0.5f};
+    vs[2].pos = {0.5f, 0.5f, 0.0f};
+    vs[3].pos = {1.0f, 0.5f, 0.5f};
+    vs[4].pos = {0.5f, 0.5f, 1.0f};
+    vs[5].pos = {0.0f, 0.5f, 0.5f};
+    Triangle t0{};
+    t0.v0 = vs[0];
+    t0.v1 = vs[4];
+    t0.v2 = vs[3];
+    t0.material_id = 3;
+    test_scene.AddTriangle(t0);
+    Triangle t1{};
+    t1.v0 = vs[0];
+    t1.v1 = vs[5];
+    t1.v2 = vs[4];
+    t1.material_id = 2;
+    test_scene.AddTriangle(t1);
+    Triangle t2{};
+    t2.v0 = vs[0];
+    t2.v1 = vs[2];
+    t2.v2 = vs[5];
+    t2.material_id = 1;
+    test_scene.AddTriangle(t2);
+    Triangle t3{};
+    t3.v0 = vs[0];
+    t3.v1 = vs[3];
+    t3.v2 = vs[2];
+    t3.material_id = 0;
+    test_scene.AddTriangle(t3);
+    Triangle t4{};
+    t4.v0 = vs[1];
+    t4.v1 = vs[3];
+    t4.v2 = vs[4];
+    t4.material_id = 3;
+    test_scene.AddTriangle(t4);
+    Triangle t5{};
+    t5.v0 = vs[1];
+    t5.v1 = vs[4];
+    t5.v2 = vs[5];
+    t5.material_id = 2;
+    test_scene.AddTriangle(t5);
+    Triangle t6{};
+    t6.v0 = vs[1];
+    t6.v1 = vs[2];
+    t6.v2 = vs[5];
+    t6.material_id = 1;
+    test_scene.AddTriangle(t6);
+    Triangle t7{};
+    t7.v0 = vs[1];
+    t7.v1 = vs[2];
+    t7.v2 = vs[3];
+    t7.material_id = 0;
+    test_scene.AddTriangle(t7);
+
+    // Lights
+    PointLight p{};
+    p.color = color::kWhite;
+    p.pos = {1.5f, 1.5f, 1.5f};
+    p.power = 1.0f;
+    test_scene.AddPointLight(p);
+
+    // Camera
+    Vec3 c_target = Vec3(0.5f);
+    Camera& c = test_scene.GetCamera();
+    c.pos = {2.5f, 2.5f, 2.5f};
+    c.dir = Normalize(c_target - c.pos);
+    c.up = CalculateCameraUp(c.pos, c_target, {0.0f, 1.0f, 0.0f});
+    c.far = 1e3;
+    c.fov = 0.6;
+
+    return test_scene;
+}
+
+}  // namespace
 
 CliApp::CliApp(AppParameters app_params) : App(app_params) {
     Logger::info("CliApp created");
